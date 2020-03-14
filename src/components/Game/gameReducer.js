3668144055785b511f185a40063effda8a9da909
Shabareesh J L <Shabareesh_JL@external.mckinsey.com>
2020-03-15 03:25:18 +0530

    Remove unused actions

--- src/components/Game/gameReducer.js
+++ src/components/Game/gameReducer.js
@@ -1,11 +1,7 @@
 import {
-    SET_RIGHT_WEIGHT_LEFT_POSITION,
-    SET_RIGHT_WEIGHT,
-    SET_RIGHT_WEIGHT_SHAPE,
-    SET_LEFT_WEIGHT,
+    SET_GAME_CONFIG,
     SET_LEFT_WEIGHT_LEFT_POSITION,
-    SET_LEFT_WEIGHT_SHAPE,
-    SET_LEFT_WEIGHT_TOP_POSITION, SET_GAME_CONFIG,
+    SET_LEFT_WEIGHT_TOP_POSITION,
 } from "./gameConstants";
 
 const gameReducer = (state = {}, action) => {
@@ -15,35 +11,10 @@ const gameReducer = (state = {}, action) => {
                 ...state,
                 ...action.data,
             };
-        case SET_RIGHT_WEIGHT_LEFT_POSITION:
-            return {
-                ...state,
-                rightWeighLeftPosition: action.data,
-            };
-        case SET_RIGHT_WEIGHT:
-            return {
-                ...state,
-                rightWeight: action.data,
-            };
-        case SET_RIGHT_WEIGHT_SHAPE:
-            return {
-                ...state,
-                rightWeightShape: action.data,
-            };
-        case SET_LEFT_WEIGHT:
-            return {
-                ...state,
-                leftWeight: action.data,
-            };
         case SET_LEFT_WEIGHT_LEFT_POSITION:
             return {
                 ...state,
                 leftWeightLeftPosition: action.data,
-            };
-        case SET_LEFT_WEIGHT_SHAPE:
-            return {
-                ...state,
-                leftWeightShape: action.data,
             };
         case SET_LEFT_WEIGHT_TOP_POSITION:
             return {
